723cf8243f3cb071fef4b8c4fbf42e6398e8dc94
segundogarden <segundogarden@gmail.com>
2026-09-03 10:31:53 -0300

    1.9

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDmfogpLSCCTUdm4TTelvCGIObFM1sVy3o",
+  authDomain: "baozeira.firebaseapp.com",
+  projectId: "baozeira",
+  storageBucket: "baozeira.firebasestorage.app",
+  messagingSenderId: "371136563090",
+  appId: "1:371136563090:web:effec8a8373d2c700be34b"
+};
